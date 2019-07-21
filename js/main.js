35c5ed90017c9140952db1614be1e9f4e203f4f1
Kaliada Andrei <Koleda05@gmail.com>
2019-07-21 16:33:10 +0200

    Сode refactoring

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import {getTrandingDay} from '/js/trandingDayResponse.js';
+import {cutText} from '/js/filmsDescription.js';
+import {addEventMedia} from '/js/addEventMedia.js';
+
 
 
 const searchForm = document.querySelector('#search-form'),
@@ -32,16 +35,23 @@ function apiSearch(event){
             let nameFilm = item.name || item.title;
             const poster = item.poster_path ? imagePoster + item.poster_path : '/image/noposter.jpg';
             let dataInfo = '';
+            let overview = item.overview;
+            let itemDate = (item.release_date !== "" && item.release_date !== undefined) ? (new
+                    Date(Date.parse(item.release_date))).toLocaleString("en", {
+                        year: 'numeric', day: 'numeric', month: 'long'
+                    }) : 'Unknown';
             if(item.media_type !== 'person') dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
             inner += `
             <div class="col-12 col-md-4 cardPoster" ${dataInfo}>
                 <div class="card">
                     <img src='${poster}' class='img_poster' alt='${nameFilm}' >
-                    <div class="cardInfo">
-                        <h5>${nameFilm}</h5>
+                    <div class="col-12 cardInfo">
+                        <h5 class="card-title text-success text-center">${nameFilm}</h5>
+                        <h6 class="text-center text-info font-weight-light">Release date: ${itemDate}</h6>
+                        <p class="text-sm-left"><small>${cutText(overview, 40, '...')}</small></p>
                     </div>
                 </div>
-            </div>    
+            </div>       
             `;
            
         });
@@ -54,76 +64,4 @@ function apiSearch(event){
 };
 
 searchForm.addEventListener('submit',apiSearch);
-
-
-function addEventMedia(){
-    const media = movies.querySelectorAll('.cardPoster[data-id]');
-    media.forEach(elem =>{
-        elem.classList.add('cardBorder');
-        elem.addEventListener('click',showFullInfo);
-    });
-};
-
-function showFullInfo(){
-    let url = '';
-    if(this.dataset.type === 'movie'){
-        url = 'https://api.themoviedb.org/3/movie/' + this.dataset.id + '?api_key=a1d9f1331776385aafc953bd762fce3b&language=en-US';
-    } else if(this.dataset.type === 'tv'){
-        url = 'https://api.themoviedb.org/3/tv/' + this.dataset.id + '?api_key=a1d9f1331776385aafc953bd762fce3b&language=en-US';
-    }else{
-     inner = `<h2 class="col-12 text-center text-info">No results found for your request</h2>`;
-    }
-    
-
-    fetch(url)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            movies.innerHTML = `
-            <div class="col-12 text-center text-info titlePosters">
-                <h2>${data.name || data.title}</h2>
-            </div>
-            <div class ="col-12">
-                <div class="row">
-                    <div class = "col-md-4 col-sm-12x">
-                        <img src="${imagePoster + data.poster_path}" alt="${data.name || data.title}" style="max-width:100%;">
-                        <div class="linksOfficial">
-                            ${(data.homepage) ? `<p><a href='${data.homepage}' target="_blank">Official page</a></p>` : ''}
-                            ${(data.imdb_id) ? `<p><a href='http://imdb.com/title/${data.imdb_id}' target="_blank">Page IMDB.com</a></p>` : ''}
-                        </div>    
-                    </div>
-                    <div class = "col-md-8 col-sm-12">
-                        <p>Rate: ${data.vote_average}</p>
-                        <p>Status: ${data.status}</p>
-                        <p>Premiere: ${data.first_air_date || data.release_date}</p>
-                        ${(data.last_episode_to_air) ? `<p>${data.number_of_seasons} season
-                        ${data.last_episode_to_air.episode_number} episodes released 
-                        </p>`:''}
-                        ${data.overview}
-                    </div>              
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <div class="youtube"></div>
-                    </div>
-                </div>    
-            </div>
-            `;
-
-            getVideo(this.dataset.type,this.dataset.id);
-        })
-        .catch(error => {
-            console.log(`Error: ${error.status}`);
-        });
-};
-
-function getVideo(type,id){
-    let youtube = movies.querySelector('.youtube');
-    //youtube.innerHTML = id;
-    youtube.innerHTML = type;
-
-}
-
 document.addEventListener('DOMContentLoaded',getTrandingDay);
